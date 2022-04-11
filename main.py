dc6688813dac40d3fb2424df3f40793a7c928c94
Константин Тен <103391773+kostyaten232@users.noreply.github.com>
2022-04-11 02:15:03 +0000

    5

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-['Doctor Who', 'Friends']
+a = 'Fight Club'
+b = 'David Fincher'           
+с = '1999'
+print(f' {a}')
+print(f'  {b}')
+print(f'  {с}')
+print('{2} {1} {0}'.format('1999', 'David Fincher', 'Fight Club'))
