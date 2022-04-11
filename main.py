606636077b99df914e697a437cbd55ba2c603ac4
Константин Тен <103391773+kostyaten232@users.noreply.github.com>
2022-04-11 00:56:23 +0000

    3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-a = 8.8618945
-b = 9            
+a = 'Fight Club'
+b = 'David Fincher'           
+с = '1999'
 print(f' {a}')
-print('%.f' % (b))
-print('%.3f' % (a))
+print(f'  {b}')
+print(f'  {с}')
+print('{2} {1} {0}'.format('1999', 'David Fincher', 'Fight Club'))
---
+++ qw.py
@@ -0,0 +1,19 @@
+# % foramatting
+print('%.3f' % (11 /  3))
+name = 'Mary'; age = 15
+print('Hi %s you are %d' % (name, age))
+
+# .format()
+print('{} {} {}'.format('Me', 'and', 'You'))
+print('{who} {} {}'.format('Me', 'and', who='You'))
+print('{who} {} {}'.format('Me', 'and', who='You'))
+print('{2} {1} {0}'.format('Me', 'and', 'You'))
+
+# f'{var]'
+a = 5
+print(f'%.{a}f' % (11 /  3))
+name = 'Jonny Cage'
+print(f'This is the end of you, {name}')
+
+
+
