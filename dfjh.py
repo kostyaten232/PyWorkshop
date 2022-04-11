dbbb90900db5751d84c49b5b584daa0982030ab3
Константин Тен <103391773+kostyaten232@users.noreply.github.com>
2022-04-11 23:08:48 +0000

    2.3

--- dfjh.py
+++ dfjh.py
@@ -46,4 +46,14 @@ def my_func(a, lst=[]):
 def my_func(a):
   lst = []
   
-    
+
+
+
+stock = []
+for i in range(7):
+  for j in range(i, 7):
+    stock.append([i, j])
+
+
+
+stock = [[i, j] for i in range(7) for j in range(i, 7)]
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-a = 'Fight Club'
-b = 'David Fincher'           
-с = '1999'
-print(f' {a}')
-print(f'  {b}')
-print(f'  {с}')
-print('{2} {1} {0}'.format('1999', 'David Fincher', 'Fight Club'))
+Here is the full syntax for slicing:
+sequence[start:stop:step]
+
+What are the default values for start, stop and step?
+start:0
+start:1
+stop:len(sequence)
+stop:len(sequence) - 1
+step:0
+step:1
+
+Put variants in answer like this [0:stop:len(sequence) - 1:1]
+
+[0:4:1]
