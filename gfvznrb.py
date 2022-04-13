4ae01541e705fe487a322e5c7278aae940e074ec
Константин Тен <103391773+kostyaten232@users.noreply.github.com>
2022-04-13 01:32:46 +0000

    7.3

---
+++ gfvznrb.py
@@ -0,0 +1,95 @@
+'''Python Workshop DAY 3
+1 Project Stage 2 (enumerate!!)
+2 Binary Search!!!
+3 Primes - new (folder 06)
+4 Exceptions and Handlers- Theory and Practice
+'''
+
+
+
+print('Hello')
+print(10 / 0)  # code goes till first exception
+
+# typical
+
+# NameError
+a = 5; print(a + b)  # b is not defined before call
+
+# TypeError
+print('15' + 2)  # operation or func is applied to OBJ of wrong type
+
+# ValueError
+print(int('five'))  # OBJ type is ok but value inappropriate
+
+# PYTHON exception tree
+# BaseException
+#  +-- SystemExit
+#  +-- KeyboardInterrupt
+#  +-- GeneratorExit
+#  +-- Exception
+#       +-- StopIteration
+#       +-- StopAsyncIteration
+#       +-- ArithmeticError
+#       |    +-- FloatingPointError
+#       |    +-- OverflowError
+#       |    +-- ZeroDivisionError
+#       +-- AssertionError
+#       +-- AttributeError
+#       +-- BufferError
+#       +-- EOFError
+#       +-- ImportError
+#       |    +-- ModuleNotFoundError
+#       +-- LookupError
+#       |    +-- IndexError
+#       |    +-- KeyError
+#       +-- MemoryError
+#       +-- NameError
+#       |    +-- UnboundLocalError
+#       +-- OSError
+#       |    +-- BlockingIOError
+#       |    +-- ChildProcessError
+#       |    +-- ConnectionError
+#       |    |    +-- BrokenPipeError
+#       |    |    +-- ConnectionAbortedError
+#       |    |    +-- ConnectionRefusedError
+#       |    |    +-- ConnectionResetError
+#       |    +-- FileExistsError
+#       |    +-- FileNotFoundError
+#       |    +-- InterruptedError
+#       |    +-- IsADirectoryError
+#       |    +-- NotADirectoryError
+#       |    +-- PermissionError
+#       |    +-- ProcessLookupError
+#       |    +-- TimeoutError
+#       +-- ReferenceError
+#       +-- RuntimeError
+#       |    +-- NotImplementedError
+#       |    +-- RecursionError
+#       +-- SyntaxError
+#       |    +-- IndentationError
+#       |         +-- TabError
+#       +-- SystemError
+#       +-- TypeError
+#       +-- ValueError
+#       |    +-- UnicodeError
+#       |         +-- UnicodeDecodeError
+#       |         +-- UnicodeEncodeError
+#       |         +-- UnicodeTranslateError
+#       +-- Warning
+#            +-- DeprecationWarning
+#            +-- PendingDeprecationWarning
+#            +-- RuntimeWarning
+#            +-- SyntaxWarning
+#            +-- UserWarning
+#            +-- FutureWarning
+#            +-- ImportWarning
+#            +-- UnicodeWarning
+#            +-- BytesWarning
+#            +-- EncodingWarning
+#            +-- ResourceWarning
+
+#NOTE!!
+
+# if using parent name it will cover ALL exception in it
+# so 
+# RuntimeError will include both exceptions in it
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-x_list = [1, 2, 3, 4] 
-y_list = [10, 11, 12, 13]
-result = list(map(lambda x, y: x + y, x_list, y_list))
-print(result)
-
-result = x_list + y_list
-print(result)
-
-
-
-
-
-
-A = [11, 13, 15, 17]
-B = [1, 2, 3, 4, 10, 11, 12, 13]
+TypeError -> int('twenty')
+ValueError -> len(42)
+ZeroDivisionError -> print(7 - 7 / 0 + 1)
