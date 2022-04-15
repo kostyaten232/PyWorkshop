b36cbe36e466b2e8494cbd06070141cb80287329
Константин Тен <103391773+kostyaten232@users.noreply.github.com>
2022-04-15 00:54:11 +0000

    6.2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-N = int (input ('Введите число: '))
+P = int (input ('Введите число: '))
  
-for d in range (1, N // 2 + 1) :
-  if N % d == 0 :
+for d in range (1, P // 2 + 1) :
+  if P % d == 0 :
     print (d, ' ', sep = '', end = '')
-print (N)
+print (P)
